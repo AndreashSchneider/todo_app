7930dfe7f0385f6fe885151aa0976eb7f0c22481
Andreas <andreas@andreas-laptop.(none)>
2011-05-14 21:09:20 +0200

    Kalendar funktioniert

--- public/javascripts/application.js
+++ public/javascripts/application.js
@@ -1,2 +1,5 @@
 // Place your application-specific JavaScript functions and classes here
 // This file is automatically included by javascript_include_tag :defaults
+    $(function (){  
+        $('#aufgabe_erfasst,#aufgabe_erledigt_am,#aufgabe_termin').datepicker({ dateFormat: 'dd.mm.yy'});  
+    });  
