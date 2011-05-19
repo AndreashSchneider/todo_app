43ae92234aa3b3f276b03caf8b7aeb6f05dcce72
Andreas <andreas@andreas-laptop.(none)>
2011-05-19 08:29:24 +0200

    Seed funktioniert

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,33 @@
 #
 #   cities = City.create([{ :name => 'Chicago' }, { :name => 'Copenhagen' }])
 #   Mayor.create(:name => 'Daley', :city => cities.first)
+
+#  json-Beispiel hat nicht funktioniert.
+# json = ActiveSupport::JSON.decode(File.read('db/seeds/aufgaben.json'))
+#   json.each do |a|
+#     Aufgabe.create!(a['aufgabe'])
+#   end
+#   json = ActiveSupport::JSON.decode(File.read('db/seeds/aufgaben.json'))  
+#   json.each do |a|
+#     Aufgabe.create!(a['Aufgabe'])
+#   end
+
+                                                                                         
+   ActiveSupport::Inflector.inflections do |inflect|
+     inflect.irregular 'aufgabe', 'aufgaben'
+     inflect.irregular 'karte', 'kartei'
+   end                                     
+
+  class Aufgabe < ActiveRecord::Base
+  end 
+
+ @aktuelles= YAML.load_file("db/seeds/akt_aufgaben")                         
+   @aktuelles.each do |task|   
+     Aufgabe.find(task[:id]).destroy if Aufgabe.exists?(task[:id])
+     aufgabe = Aufgabe.new         
+     task.attribute_names.each do |attr| 
+      eval "aufgabe.#{attr}=task.#{attr}"
+     end      
+     aufgabe.save  
+     p "Aufgabe mit id=#{aufgabe.id} geschrieben"       
+   end
