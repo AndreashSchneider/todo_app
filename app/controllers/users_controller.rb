4a3b0553f54c027555e32acf980093d4b1fa5184
Andreas <andreas@andreas-laptop.(none)>
2011-05-21 15:03:25 +0200

    Beim Anlegen eines Users die show User-Seite anzeigen

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -10,7 +10,7 @@ class UsersController < ApplicationController
   def create   
     @user= User.new(params[:user])
     if @user.save
-      # erfolgreiches Speichern behandeln
+     redirect_to @user
     else
      @title = 'Sign up'
      render 'new'
