b90de5c6f985eea51db43d551d3d121311cb439f
Andreas <andreas@andreas-laptop.(none)>
2011-05-21 10:52:22 +0200

    Validierung User-email und Index auf email

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,2 +1,12 @@
 class User < ActiveRecord::Base
+  attr_accessible :name, :email
+
+  email_regex =/\A[\w+\-.]+@[a-z\d\-.]+\.[a-z]+\z/i
+
+  validates (:name, :presence => true,
+                    :lenght => {:maximum => 50})
+  validates (:email, :presence => true,
+                     :formati => {with => email_regex})
+ 
+
 end
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended to check this file into your version control system.
 
-ActiveRecord::Schema.define(:version => 20110514123052) do
+ActiveRecord::Schema.define(:version => 20110521085116) do
 
   create_table "aufgaben", :force => true do |t|
     t.date     "erfasst"
@@ -36,6 +36,13 @@ ActiveRecord::Schema.define(:version => 20110514123052) do
 
   create_table "projekte", :force => true do |t|
     t.string   "title"
+    t.datetime "created_at"
+    t.datetime "updated_at"
+  end
+
+  create_table "users", :force => true do |t|
+    t.string   "name"
+    t.string   "email"
     t.datetime "created_at"
     t.datetime "updated_at"
   end
