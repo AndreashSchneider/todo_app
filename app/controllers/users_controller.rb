e416a36bb1ec238af4a061d31a8233ce3cad72c5
Andreas <andreas@andreas-laptop.(none)>
2011-05-24 20:50:41 +0200

    User können  nun editiert werden.

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,7 +1,23 @@
 class UsersController < ApplicationController
-  
+  before_filter :authenticate, :only=>[:edit, :update]
   def show
    @user = User.find(params[:id])
+  end
+
+  def edit
+    @user = User.find(params[:id])
+    @title= "Edit User"
+  end
+
+  def update
+    @user = User.find(params[:id])
+    if @user.update_attributes(params[:user])
+      flash[:success]="Profil updated."
+      redirect_to @user
+    else 
+      @title ="Edit user"
+      render 'edit'
+    end
   end
 
   def new
@@ -18,4 +34,8 @@ class UsersController < ApplicationController
      render 'new'
     end
   end  
+ private 
+   def authenticate 
+     deny_access unless signed_in?
+   end
 end
