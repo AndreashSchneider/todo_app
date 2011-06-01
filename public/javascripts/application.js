6f95696108af95f5887876a8f45600f478c464d3
Andreas <andreas@andreas-laptop.(none)>
2011-06-01 19:39:32 +0200

    mit +und-

--- public/javascripts/application.js
+++ public/javascripts/application.js
@@ -1,5 +1,17 @@
 // Place your application-specific JavaScript functions and classes here
 // This file is automatically included by javascript_include_tag :defaults
+
     $(function (){  
         $j('#aufgabe_erfasst,#aufgabe_erledigt_am,#aufgabe_termin,#erfasst,#erledigt_am,#termin,#zuletzt_bearbeitet').datepicker({ dateFormat: 'yy-mm-dd'});  
     });  
+// Detailsuche  Ein-/Ausblenden
+var $j = jQuery.noConflict();
+jQuery(document).ready(function ($){
+  $j(".detail_suche").hide("fast");$j("#button_detail_aus").hide("fast")
+  $j("#button_detail_aus").click(function () {$j(".detail_suche").hide("fast"); $j("#button_detail_aus").hide("fast"); $j("#button_detail_ein").show("fast"); });
+  $j("#button_detail_ein").click(function () {$j(".detail_suche").show("fast"); $j("#button_detail_ein").hide("fast"); $j("#button_detail_aus").show("fast"); });
+   });;
+
+
+        
+
