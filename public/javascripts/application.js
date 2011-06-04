1538b425b8edd7d0451a619efc03a071141d884b
Andreas <andreas@andreas-laptop.(none)>
2011-06-04 09:52:25 +0200

    alle ZusatzInfos in PopUp

--- public/javascripts/application.js
+++ public/javascripts/application.js
@@ -6,11 +6,11 @@
     });  
 // Detailsuche  Ein-/Ausblenden
 var $j = jQuery.noConflict();
-jQuery(document).ready(function ($){
-  $j(".detail_suche").hide("fast");$j("#button_detail_aus").hide("fast")
-  $j("#button_detail_aus").click(function () {$j(".detail_suche").hide("fast"); $j("#button_detail_aus").hide("fast"); $j("#button_detail_ein").show("fast"); });
-  $j("#button_detail_ein").click(function () {$j(".detail_suche").show("fast"); $j("#button_detail_ein").hide("fast"); $j("#button_detail_aus").show("fast"); });
-   });;
+//jQuery(document).ready(function ($){
+//  $j(".detail_suche").hide("fast");$j("#button_detail_aus").hide("fast")
+//  $j("#button_detail_aus").click(function () {$j(".detail_suche").hide("fast"); $j("#button_detail_aus").hide("fast"); $j("#button_detail_ein").show("fast"); });
+ // $j("#button_detail_ein").click(function () {$j(".detail_suche").show("fast"); $j("#button_detail_ein").hide("fast"); $j("#button_detail_aus").show("fast"); });
+//   });;
 
 
         
